6deead02ac37c6f25d61b8eb175573a58f146cf7
MacmeDan <pdanielleonard@gmail.com>
2017-03-19 16:36:39 -0600

    remove delete call at beginning.

--- StarWarsDirectory/AppDelegate.swift
+++ StarWarsDirectory/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        PersistedData.shared?.deleteAllCharicters()
+        //PersistedData.shared?.deleteAllCharicters()
         syncCharacters()
         return true
     }
---
+++ StarWarsDirectory/UIImage.swift
@@ -0,0 +1,9 @@
+//
+//  UIImage.swift
+//  StarWarsDirectory
+//
+//  Created by P D Leonard on 3/19/17.
+//  Copyright © 2017 MacMeDan. All rights reserved.
+//
+
+import Foundation
